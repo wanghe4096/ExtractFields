d670b04e65cc31bcb4329b98d49d99b0f1c7cab6
candythinking <candythinking@126.com>
2016-03-25 17:56:03 +0800

    merge multiline proc to t_extract.py

--- t_extract.py
+++ t_extract.py
@@ -8,9 +8,10 @@
         eg.
             python t_extract.py etc access_combined
 
-    3  给定 source_type　和　具体的日志文件，　显示解析后的结果
+    3  给定 source_type　和　具体的日志文件，　会有两个方法进行选择，1 表示选择extract方法 ：显示解析后的结果，2 表示选择multiline方法：多行event进行合并,并显示合并的过程和结果
         eg.
-            python t_extract.py etc access_combined ../LogSample/apache/access_log
+            单行:python t_extract.py etc access_combined samples/apache.log
+            多行:python t_extract.py etc catalina samples/ANON-catalina.log
 
 """
 import os
@@ -18,6 +19,74 @@ import sys
 import re
 import spl_common
 from jinja2 import Environment, FileSystemLoader
+
+
+def multiline(sourcetype,props_conf,logfile):
+    sourcetype="catalina"
+    print '1.read file as follow:'
+    print 'props_conf'
+    log = open(logfile)
+    print logfile,'===>>>open'
+
+
+    print '\n\n\n2.读取props文件节的默认配置'
+    sourcetype_stanza_props = props_conf['default']
+    print sourcetype_stanza_props
+
+
+    print '\n\n\n3.获得sourcetype所对应的props文件节的配置覆盖默认的配置'
+    for (k,v) in props_conf[sourcetype].items():
+        sourcetype_stanza_props[k] = v
+    print props_conf[sourcetype],"\n===覆盖>>>result===>>>\n",sourcetype_stanza_props
+ 
+
+    print '\n\n\n4.当SHOULD_LINEMERGE为true时，说明需要进行多行构建，否则不需要'
+    event={"sourcetype":sourcetype,"_raw":""}
+    multiline_re = r'(\d+:\d+:\d)'
+    print '默认的时间戳正则:',multiline_re
+    print 'SHOULD_LINEMERGE:',sourcetype_stanza_props['SHOULD_LINEMERGE']
+    if sourcetype_stanza_props['SHOULD_LINEMERGE']=='true':
+        print '判断多行构建的正则，如果存在BREAK_ONLY_BEFORE，就用它，没有就默认用BREAK_ONLY_BEFORE_DATE'
+        print 'BREAK_ONLY_BEFORE:',sourcetype_stanza_props['BREAK_ONLY_BEFORE'],':在此冒号之前为值'
+        if sourcetype_stanza_props['BREAK_ONLY_BEFORE']:
+            print multiline_re,'===>>>',sourcetype_stanza_props['BREAK_ONLY_BEFORE']
+            multiline_re = sourcetype_stanza_props['BREAK_ONLY_BEFORE']
+
+        print '当LINE_BREAKER不存在时为默认的\\r\\n'
+        print 'sourcetype_stanza_props.has_key("LINE_BREAKER"):',sourcetype_stanza_props.has_key("LINE_BREAKER")
+
+
+        if not sourcetype_stanza_props.has_key("LINE_BREAKER"):
+            linecount = 0
+            count = 0
+            print '单行读取进行多行event构建'
+            for line in log:
+                if re.search(multiline_re,line):
+                    if linecount >= 1:
+                        print '===============事件结束==============='
+                        event['linecount'] = linecount
+                        #print event
+                        event={"sourcetype":sourcetype,"_raw":""}
+                    linecount = 1
+                    count = count + 1
+                    event['_raw']=str(count)+'\t'+line
+                    print '\n\n\n===============事件开始===============\n',event['_raw']
+                else:
+                    linecount = linecount + 1
+                    count = count + 1
+                    line = str(count)+'\t'+line
+                    event['_raw']=event['_raw']+line
+                    print "===========多行合并 " + str(linecount-1) +" 次===========\n"
+                    print event['_raw']
+            print '===============事件结束==============='
+    else:
+        print "不需要多行构建，一个LINE_BREAKER分割即为一个event"
+        for line in log:
+            event={"linecount":1,"sourcetype":sourcetype,"_raw":line}
+            print event
+        print "不需要多行构建，一个LINE_BREAKER分割即为一个event"
+    log.close()
+    print logfile,'===>>>close'
 
 
 def list_all_source_type(source_types, props):
@@ -190,6 +259,12 @@ if __name__ == '__main__':
         # <bin>, <conf_path> <source_type> <log_file>,  list the source type's execute plan
         source_type = sys.argv[2]
         log_file = sys.argv[3]
-        extract(source_types, source_type, props, transforms, log_file)
+        select_method = raw_input("please select method(1.extract,2.multiline): ")
+        if select_method == '1' or select_method == 'extract':
+            extract(source_types, source_type, props, transforms, log_file)
+        elif select_method == '2' or select_method == 'multiline':
+            multiline(source_type,props,log_file)
+        else:
+            print 'please input: 1 | extract | 2 | multiline'
 
 # end of file
