0149b26e710c735e3f300ef646732db8e4cdeb94
candythinking <candythinking@126.com>
2016-04-13 11:18:05 +0800

    整合pcre

--- sourcetype_lua/oneapm.lua
+++ sourcetype_lua/oneapm.lua
@@ -1,15 +1,15 @@
 
 --49444250/104.022794008=475321.30310014
 --49444250/125.411713839=394255.4366450579
-
-local namestype = {status='int',client_ip='string',server_ip='string',client_dev_message='string',timestamp='string',bytes='int',uri='string',uri__domain='string',uri_path='string',uri_='string',version='string',uri_query='string',server_port='int',file='string',response_size='int',root='string',method='string',response_time='double'}
+local oneapm = {}
+oneapm.namestype = {status='int',client_ip='string',server_ip='string',client_dev_message='string',timestamp='string',bytes='int',uri='string',uri__domain='string',uri_path='string',uri_='string',version='string',uri_query='string',server_port='int',file='string',response_size='int',root='string',method='string',response_time='double'}
 -- build series transform | report | extract
 -- report
 
 
 
 local reg = "^(?<client_ip>\\S+)\\s(?<server_ip>\\S+):(?<server_port>\\d+)\\s(?<response_time>\\d*\\.\\d+|(?:0x[a-fA-F0-9]+|\\d+))\\s-\\s\\[(?<timestamp>[^\\]]*+)\\]\\s(?:\"\\s*+(?<method>[^\\s\"]++)?(?:\\s++(?:(?<uri>(?<uri_>(?<uri__domain>\\w++:\\/\\/[^\\/\\s\"]++))?+(?<uri_path>(?:\\/++(?<root>(?:\\\\\"|[^\\s\\?\\/\"])++)\\/++)?(?:(?:\\\\\"|[^\\s\\?\\/\"])*+\\/++)*(?<file>[^\\s\\?\\/]+)?)(?:\\?(?<uri_query>[^\\s]*))?))(?:\\s++(?<version>[^\\s\"]++))*)?\\s*+\")\\s(?<status>\\d+)\\s(?<response_size>\\d+)\\s(?<bytes>\\d+)\\s\\\"-\\\"\\s(?<client_dev_message>.*)"
-local names = {'client_ip','server_ip','server_port','response_time','timestamp','method','uri','uri_','uri__domain','uri_path','root','file','uri_query','version','status','response_size','bytes','client_dev_message'}
+oneapm.names = {'client_ip','server_ip','server_port','response_time','timestamp','method','uri','uri_','uri__domain','uri_path','root','file','uri_query','version','status','response_size','bytes','client_dev_message'}
 local n = 18
 
 
@@ -83,35 +83,44 @@ function DefaultLineBreakerFeed:finished(data)
 end
 
 
-local feeder = DefaultLineBreakerFeed:new()
+oneapm.feeder = DefaultLineBreakerFeed:new()
 
 
 local ffi = require('ffi')
-require('libpcre.pcre_header')
-local L = ffi.load('pcre')
+ffi.cdef[[
+typedef struct real_pcre pcre;
+typedef struct pcre_extra pcre_extra;
+static const int PCRE_STUDY_JIT_COMPILE = 0x00000001;
+pcre *pcre_compile(const char *, int, const char **, int *,
+                  const unsigned char *);
+pcre *pcre_compile2(const char *, int, int *, const char **,
+                  int *, const unsigned char *);
+pcre_extra *pcre_study(const pcre *, int, const char **);
+int pcre_exec(const pcre *, const pcre_extra *, const char *,
+                   int, int, int, int *, int);
+void pcre_free_study(pcre_extra *);
+void (*pcre_free)(void *);
+]]
+local pcre = ffi.load('pcre')
 local errptr = ffi.new('const char*[1]')
 local intptr = ffi.new('int[1]')
-local re = L.pcre_compile(reg, 0, errptr, intptr, nil)
+local re = pcre.pcre_compile(reg, 0, errptr, intptr, nil)
 
 local size = (n + 1) * 3
 local ovector = ffi.new('int['..size..']')
-local re_stu = L.pcre_study(re, L.PCRE_STUDY_JIT_COMPILE, errptr)
+local re_stu = pcre.pcre_study(re, pcre.PCRE_STUDY_JIT_COMPILE, errptr)
 local ret = {}
 local st = n + 1 
 
 
 function match(subject, regex)
-  L.pcre_exec(re, re_stu, subject, #subject, 0, 0, ovector, size)
+  pcre.pcre_exec(re, re_stu, subject, #subject, 0, 0, ovector, size)
   for i=0, n*2, 2 do
     if ovector[i] >= 0 then
       ret[i/2] = subject:sub(ovector[i]+1, ovector[i+1])
     end
   end
   return ret
-end
-
-function string.ends(String,End)
-   return End=='' or string.sub(String,-string.len(End))==End
 end
 
 
@@ -130,31 +139,14 @@ local f = io.open(data_file, "rb")
 while true do
     local block = f:read(buffer_size)
     if not block then
-        feeder:finished()
+        oneapm.feeder:finished()
         break
     end
-    -- io.write(block)
-    feeder:feed(block)
+    oneapm.feeder:feed(block)
 end
 f:close()
---pcre.L.pcre_free_study(pcre.re_stu)
---pcre.L.pcre_free(pcre.re)
-
+--pcre.pcre_free_study(pcre.re_stu)
+--pcre.pcre_free(pcre.re)
 module(...)
-
--- read buffer from stdin
---local buffer_size = 2^16    -- make a 64k buffer. use this buffer make reading faster.
---local f = io.open(data_file, "rb")
---while true do
---    local block = f:read(buffer_size)
---    if not block then
---        feeder:finished()
---        break
---    end
---    feeder:feed(block)
---end
---f:close()
---pcre.L.pcre_free_study(pcre.re_stu)
---pcre.L.pcre_free(pcre.re)
-
+return oneapm
 -- end of file
